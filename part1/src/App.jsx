e65ba94eb7838ff576bea5665ac63e3542d56fa2
Derek L <derekluciani@gmail.com>
2026-05-09 23:14:36 -0400

    part1.D1 added

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -4,18 +4,36 @@ import { useState } from 'react'
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
+  //Every click is stored in a separate piece of state called allClicks that is initialized as an empty array
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
+  const handleLeftClick = () => {
+    //When the left button is clicked, we add the letter L to the allClicks array
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+    setTotal(left + right)
+  }
+  const handleRightClick = () => {
+    //the concat method, does not mutate the existing array but rather returns a new copy with the item added to it.
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+    setTotal(left + right)
+  }
   return (
-    <div>
-      {left}
-      <button onClick={() => setLeft(left + 1)}>
-        left
-      </button>
-      <button onClick={() => setRight(right + 1)}>
-        right
-      </button>
-      {right}
-    </div>
+    <>
+      <div className="buttons">
+        {left}
+        <button onClick={handleLeftClick}>left</button>
+        <button onClick={handleRightClick}>right</button>
+        {right}
+      </div>
+      <div>
+        {/* join method joins all items into a single string, separated by the string passed as the function param.*/}
+        <p>{allClicks.join(' ')}</p>
+        <p>total clicks: {total}</p>
+      </div>
+    </>
   )
 }
 
